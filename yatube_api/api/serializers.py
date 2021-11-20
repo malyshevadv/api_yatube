8266b0879fad5e05d0b1edddad793cd7c0e85495
Darya <lunaticmoony13@gmail.com>
2021-11-20 17:30:13 +0300

    Sorry it took so long

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from posts.models import Comment, Group, Post
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    group = serializers.PrimaryKeyRelatedField(
+        required=False,
+        queryset=Group.objects.all()
+    )
+    author = serializers.StringRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    image = serializers.ImageField(
+        max_length=None,
+        allow_empty_file=True,
+        required=False
+    )
+
+    class Meta:
+        model = Post
+        fields = ('id', 'text', 'author', 'image', 'pub_date', 'group')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'author', 'created', 'post')
---
+++ yatube_api/api/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, viewsets
+from posts.models import Group, Post
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `owner`.
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly, permissions.IsAuthenticated)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrReadOnly, permissions.IsAuthenticated)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get("post_id")
+        post = get_object_or_404(Post, pk=post_id)
+
+        comments_list = post.comments.all()
+        return comments_list
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get("post_id")
+        post = get_object_or_404(Post, pk=post_id)
+        serializer.save(author=self.request.user, post=post)
